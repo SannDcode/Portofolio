27c1868f30770c3958137bcfea87384dee6f9ffe
Sanndcode <maulanaabdul1354@gmail.com>
2025-05-19 16:47:48 +0700

    Assesment dibimbing

---
+++ assets/JS/notifHome.js
@@ -0,0 +1,10 @@
+var hai = 'Haii!!👋🏼 :> ';
+    hicwok = 'alo bng!👊🏼 :D';
+    sory = 'Maaf, isi portofolio saya masih dalam tahap pengembangan \ndan bahasa yang di gunakan masih campuran. \nBerikut pengembangan website yang belum di selesaikan: \n=>> Responsive ❌ \n=>> Halaman Project ❌';
+    quest = '🗣 Kenapa website ini tampilannya sederhana? \n👤 Karena saya membuatnya dengan CSS Native (biasa) tanpa framework apapun. Dan saya juga menggunakan JavaScript Vanilla (biasa) tanpa framework apapun.'
+    wlcm = '\nWelcome to my portfolio';
+    your = prompt('What is your name?');
+    alert(hai + your + wlcm);
+    alert(sory);
+    alert(quest);
+    // play = confirm(your + '!! ' + 'want to listen to music?');
